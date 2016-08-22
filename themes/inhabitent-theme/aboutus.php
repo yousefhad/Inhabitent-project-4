83f72f6e56b544522fabde062649bb467687b392
yousefhad <yousefhad@yahoo.com>
2016-08-21 23:19:16 -0700

    third commit

---
+++ themes/inhabitent-theme/aboutus.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template Name: AboutUs
+ *
+ * @package RED_Starter_Theme
+ */
+?>
+<?php get_header(); ?>
+
+	<div id="content" class="site-content">
+
+   <div id="primary" class="content-area">
+      <main id="main" class="aboutmain" role="main">
+
+         <article  class="hentry">
+            <header class="entry-header hero">
+               <div class="container">
+                  <h1 class="en-title">About</h1>               </div>
+            </header>
+
+            <div class="container">
+               <div class="entry-content">
+                  <h2>Our Story</h2>
+                  <p class="p">
+                  <span class="sp">Inhabitent Camping Supply Co. has been Vancouver baked-good icon for more than two whole months! Customers often comment on the bustle of activity they see in store…that’s where the magic happens every day.
+                  </span></p>
+                  <p class="p">
+                  <span class="sp">We want to bridge the gap between the comfort of city life and the lovely Instagram-worthiness of the great outdoors that surround us. We sell gear that’s fun and functional. So much fun, in fact, that you’ll want to pitch a tent inside your one-bedroom apartment so you can use it everyday.
+                  </span></p>
+
+                  <h2>Our Team</h2>
+                  <p class="p"><span class="sp">Inhabitent Camping Supply Co.’s staff is made up of an amazing team of inspired retail associates. We really know our stuff when it comes to travel hammocks and campfire cooking gadgets. From a provincial park campground to the back-country, our staff knows what you need to outfit your outdoor outing. </span></p>
+                  <p class="p">
+                  <span class="sp">Our shop in nestled away in a lovely little corner of Vancouver. Pop in, say hi, and try out our tents!</span> </p>
+               </div>
+            </div>
+         </article>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+    </div>
+
+<?php get_footer(); ?>
--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -65,5 +65,5 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-<?php get_sidebar();?>
+
 <?php get_footer(); ?>
